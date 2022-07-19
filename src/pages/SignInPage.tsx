b15f88536694abced3f6da6937c4f3ac0ee1591d
Arasto Sahbaei <arasto.sahbaei@gmail.com>
2022-07-19 10:14:09 +0200

    added signin

--- src/pages/SignInPage.tsx
+++ src/pages/SignInPage.tsx
@@ -1,3 +1,25 @@
+import { useState } from 'react'
+import BitupService from '../shared/api/services/BitupService'
+
 export const SignInPage: React.FC = () => {
-	return <div>SignInPage</div>
+	const [email, setEmail] = useState('')
+	const [password, setPassword] = useState('')
+
+	const roflmao = async () => {
+		try {
+			const { data } = await BitupService.login({ email: email, password: password })
+			console.log(data)
+		} catch (error) {
+			console.log(error)
+		}
+	}
+
+	return (
+		<div>
+			<h1>test</h1>
+			<input placeholder='email' onChange={(event) => setEmail(event.target.value)} />
+			<input placeholder='password' type='password' onChange={(event) => setPassword(event.target.value)} />
+			<button onClick={() => roflmao()}>Logga in</button>
+		</div>
+	)
 }
