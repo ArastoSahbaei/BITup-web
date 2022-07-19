208e52de678835feddb541ebe1c765bd75101ed2
Arasto Sahbaei <arasto.sahbaei@gmail.com>
2022-07-19 10:22:01 +0200

    signin

--- src/pages/SignInPage.tsx
+++ src/pages/SignInPage.tsx
@@ -1,13 +1,21 @@
 import { useState } from 'react'
 import BitupService from '../shared/api/services/BitupService'
 
-export const SignInPage: React.FC = () => {
-	const [email, setEmail] = useState('')
-	const [password, setPassword] = useState('')
+interface LoginCredentials {
+	email: string
+	password: string
+}
 
-	const roflmao = async () => {
+export const SignInPage: React.FC = () => {
+	const [loginCredentials, setLoginCredentials] = useState<LoginCredentials>({ email: '', password: '' })
+
+	const handleChange = (event: React.ChangeEvent<HTMLInputElement>, target: keyof LoginCredentials) => {
+		setLoginCredentials({ ...loginCredentials, [target]: event.target.value })
+	}
+
+	const signIn = async () => {
 		try {
-			const { data } = await BitupService.login({ email: email, password: password })
+			const { data } = await BitupService.login({ email: loginCredentials.email, password: loginCredentials.password })
 			console.log(data)
 		} catch (error) {
 			console.log(error)
@@ -17,9 +25,9 @@ export const SignInPage: React.FC = () => {
 	return (
 		<div>
 			<h1>test</h1>
-			<input placeholder='email' onChange={(event) => setEmail(event.target.value)} />
-			<input placeholder='password' type='password' onChange={(event) => setPassword(event.target.value)} />
-			<button onClick={() => roflmao()}>Logga in</button>
+			<input placeholder='email' onChange={(event) => handleChange(event, 'email')} />
+			<input placeholder='password' type='password' onChange={(event) => handleChange(event, 'password')} />
+			<button onClick={() => signIn()}>Logga in</button>
 		</div>
 	)
 }
