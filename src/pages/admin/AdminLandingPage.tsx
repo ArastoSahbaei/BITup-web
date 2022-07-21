9241e77e5e09131a3033d03381b3a618b15a2ac4
Arasto Sahbaei <arasto.sahbaei@gmail.com>
2022-07-21 19:45:39 +0200

    m

--- src/pages/admin/AdminLandingPage.tsx
+++ src/pages/admin/AdminLandingPage.tsx
@@ -6,7 +6,7 @@ export const AdminLandingPage: React.FC = () => {
 	return (
 		<div>
 			<h1>1. send invite to new user(register)</h1>
-			<button>send invite</button>
+			<button onClick={() => createAccountAndSendInvite()}>send invite</button>
 		</div>
 	)
 }
--- src/shared/interfaces/UserInterface.ts
+++ src/shared/interfaces/UserInterface.ts
@@ -10,3 +10,20 @@ export interface IloginCredentials {
   email: string
   password: string
 }
+
+export interface IcreateAccount {
+  email: string
+  storeID: string,
+  personalDetails: IpersonalDetails
+}
+
+interface IpersonalDetails {
+  firstName: string,
+  lastName: string,
+  country: string,
+  address: string,
+  secondaryAdress: string,
+  zipCode: string,
+  phone: string,
+  secondaryPhone: string
+}
