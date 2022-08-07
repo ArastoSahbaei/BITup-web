e6cd3c1241456c39c43b216ac9adc989f3a04901
Arasto Sahbaei <arasto.sahbaei@gmail.com>
2022-08-07 15:37:21 +0200

    soc

--- src/pages/sign-in/SignInPage.tsx
+++ src/pages/sign-in/SignInPage.tsx
@@ -1,47 +1,23 @@
-import { Button } from 'components/common'
-import { useState } from 'react'
 import { primaryColor } from 'shared/styles/GlobalStyles'
+import { SignInForm } from './components/SignInForm'
+import { useState } from 'react'
 import styled from 'styled-components'
-import { useAuthentication } from '../../hooks'
-import { IloginCredentials } from '../../shared/interfaces'
 
 export const SignInPage: React.FC = () => {
-	const [loginCredentials, setLoginCredentials] = useState<IloginCredentials>({ email: '', password: '' })
 	const [recoverAccount, setRecoverAccount] = useState<string>('')
-	const { login, retrieveLostAccount } = useAuthentication()
-
-	const handleChange = (event: React.ChangeEvent<HTMLInputElement>, target: keyof IloginCredentials) => {
-		setLoginCredentials({ ...loginCredentials, [target]: event.target.value })
-	}
 
 	return (
-		<>
-			<Wrapper>
-				<span>TransBit</span>
-				<input placeholder='email' onChange={(event) => handleChange(event, 'email')} />
-				<input placeholder='password' type='password' onChange={(event) => handleChange(event, 'password')} />
-				<Button text={'Logga in'} onClick={() => login({ email: loginCredentials.email, password: loginCredentials.password })} />
-				<span>Glömt lösenord?</span>
-				<span>Privacy policy</span>
-				<br />
-			</Wrapper>
+		<Background>
+			<SignInForm />
 			<span>Dont have an account? Sign up and get started!</span>
-		</>
+		</Background>
 	)
 }
 
-const Wrapper = styled.div`
-	width: 500px;
-	margin: 0 auto;
-	padding: 6%;
-	border: 3px solid ${primaryColor};
-	border-radius: 5%;
-	margin-top: 4%;
-	transition: 0.2s;
-	&:hover {
-		box-shadow: rgb(4 17 29 / 25%) 0px 0px 20px 20px;
-		transition: 0.5s;
-	}
+const Background = styled.div`
+	background-color: red;
+	width: 100%;
+	height: 100%;
 `
 
 {
---
+++ src/pages/sign-in/components/SignInForm.tsx
@@ -0,0 +1,42 @@
+import { Button } from 'components/common'
+import { useState } from 'react'
+import { primaryColor } from 'shared/styles/GlobalStyles'
+import { useAuthentication } from 'hooks'
+import { IloginCredentials } from 'shared/interfaces'
+import styled from 'styled-components'
+
+export const SignInForm = () => {
+	const { login } = useAuthentication()
+
+	const [loginCredentials, setLoginCredentials] = useState<IloginCredentials>({ email: '', password: '' })
+
+	const handleChange = (event: React.ChangeEvent<HTMLInputElement>, target: keyof IloginCredentials) => {
+		setLoginCredentials({ ...loginCredentials, [target]: event.target.value })
+	}
+
+	return (
+		<Wrapper>
+			<span>TransBit</span>
+			<input placeholder='email' onChange={(event) => handleChange(event, 'email')} />
+			<input placeholder='password' type='password' onChange={(event) => handleChange(event, 'password')} />
+			<Button text={'Logga in'} onClick={() => login({ email: loginCredentials.email, password: loginCredentials.password })} />
+			<span>Glömt lösenord?</span>
+			<span>Privacy policy</span>
+			<br />
+		</Wrapper>
+	)
+}
+
+const Wrapper = styled.div`
+	width: 500px;
+	margin: 0 auto;
+	padding: 6%;
+	border: 3px solid ${primaryColor};
+	border-radius: 5%;
+	margin-top: 4%;
+	transition: 0.2s;
+	&:hover {
+		box-shadow: rgb(4 17 29 / 25%) 0px 0px 20px 20px;
+		transition: 0.5s;
+	}
+`
