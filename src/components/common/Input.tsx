e0abfd5b163c5af2d119a38f23c6bead52752f6f
Arasto Sahbaei <arasto.sahbaei@gmail.com>
2022-08-11 22:33:15 +0200

    added formik

--- src/components/common/Input.tsx
+++ src/components/common/Input.tsx
@@ -1,4 +1,8 @@
-import { withStyles } from '@material-ui/core/styles'
+export const Input = () => {
+	return <div>Input</div>
+}
+
+/* import { withStyles } from '@material-ui/core/styles'
 import TextField from '@mui/material/TextField'
 import styled from 'styled-components'
 
@@ -38,3 +42,4 @@ export const Input: React.FC<Props> = (props: Props) => {
 const Wrapper = styled.div`
 	padding-top: 12px;
 `
+ */
