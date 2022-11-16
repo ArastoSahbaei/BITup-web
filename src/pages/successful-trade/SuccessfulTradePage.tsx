ba22d9a4ded19f17dcc51b282aea3397157af5b3
Arasto Sahbaei <arasto.sahbaei@gmail.com>
2022-11-16 20:54:33 +0100

    offer receipt

--- src/pages/successful-trade/SuccessfulTradePage.tsx
+++ src/pages/successful-trade/SuccessfulTradePage.tsx
@@ -5,7 +5,7 @@ export const SuccessfulTradePage: React.FC = () => {
 	//TODO: ❌ - 3. If the invoiceID is true and does not have a previous sell-order on binance then go to step 5.
 	//TODO: ❌ - 4. if the invoiceID is false - what to do?
 	//TODO: ❌ - 5. Call the backend API that calls the Binance API - create a BTC-(USD?) sell order on binance with the amount of the trade.
-	//TODO: ❌ - 6. If the sell order is successful Re-direct back to application. (EVERYTHING WENT AS PLANNED)
+	//TODO: ❌ - 6. If the sell order is successful Re-direct back to page that offers receipt.
 	//TODO: ❌ - 7. If the sell order is declined - handle it in some way.
 
 	return (
