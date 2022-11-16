0ba2400ec8dda314d412547d677188f67a20f9cd
Arasto Sahbaei <arasto.sahbaei@gmail.com>
2022-11-16 20:57:30 +0100

    get params

--- src/pages/successful-trade/SuccessfulTradePage.tsx
+++ src/pages/successful-trade/SuccessfulTradePage.tsx
@@ -1,6 +1,6 @@
 export const SuccessfulTradePage: React.FC = () => {
 	// ✅❌
-	//TODO: ❌ - 1. You get re-direct here after a successful trade. You need to get the trade InvoiceID from the URL.
+	//TODO: ❌ - 1. You get re-direct here after a successful trade. You need to get the trade InvoiceID from the URI params.
 	//TODO: ❌ - 2. You need to verify that the InvoiceID is true (so people dont mock fake trades).
 	//TODO: ❌ - 3. If the invoiceID is true and does not have a previous sell-order on binance then go to step 5.
 	//TODO: ❌ - 4. if the invoiceID is false - what to do?
